c23b4a1dc5c2ab1cd21e8cd0d0beabd4cfe9de0f
oyaoya <1156029380@qq.com>
2024-06-10 11:20:36 +0800

    u

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import random
 import time
 
@@ -9,47 +10,50 @@ from TKHandler import get_questionId
 from get_questions import GetQuestionDetailForH5
 
 
-with open("./save_dir/record.json","r",encoding="utf-8") as f:
-    for planid in f.readlines():
+with open("./paperid_all.json","r",encoding="utf-8") as f:
+    for line in tqdm.tqdm(f.readlines()):
+        json_line=json.loads(line)
         # planid就是tikuid
-        paperList=get_tiku(planid)
-        paperid_ls=[e["paperID"] for each in paperList for e in each["paperList"]]
-        paperid_ls=list(filter(lambda each: None if each=="" else each,paperid_ls))
+        paperid_ls=json_line["paperId_ls"]
+        planid=json_line["planid"]
         # paperid是这张卷子的id
-
-
         for paperid in tqdm.tqdm(paperid_ls):
+            os.makedirs(f"./save_dir/{planid}",exist_ok=True)
+
+
+            file_name=f"./save_dir/{planid}/{paperid}.json"
+            if os.path.exists(file_name):
+                continue
             flag,questionId_ls=get_questionId(planid,paperid)
             # 遍历每一章
 
-            with open("./paperid_questionId_ls.json","a",encoding="utf-8") as f:
+            with open(file_name,"w",encoding="utf-8") as f:
                 f.write(json.dumps(
                     {
                         "paperid":paperid,
                         "questionId_ls":questionId_ls
                     }
                 ,ensure_ascii=False))
-                f.write("\n")
 
-            time.sleep(random.randint(3,5))
+            # time.sleep(random.randint(3,5))
 
 
-            # dataid就是questionid
-            # if not flag:
-            #     continue
-            # for questionId in tqdm.tqdm(questionId_ls,desc=paperid):
-            #     flag,result=GetQuestionDetailForH5(tkid=planid,paperid=paperid,qid=questionId)
-            #     #
-            #     if not flag:
-            #         continue
-            #     result={
-            #         "QuestionContent":result["QuestionContent"],
-            #         "QuestionAnalysis":result["QuestionAnalysis"],
-            #         "selectAnswer":result["selectAnswer"],
-            #         "paperid":paperid,
-            #         "planid":planid,
-            #         "questionId":questionId
-            #     }
-            #     with open("./xiti.jsonl","a",encoding="utf-8") as f:
-            #         f.write(json.dumps(result,ensure_ascii=False))
-            #         f.write("\n")
+        # dataid就是questionid
+        # if not flag:
+        #     continue
+        # for questionId in tqdm.tqdm(questionId_ls,desc=paperid):
+        #     flag,result=GetQuestionDetailForH5(tkid=planid,paperid=paperid,qid=questionId)
+        #     #
+        #     if not flag:
+        #         continue
+        #     result={
+        #         "QuestionContent":result["QuestionContent"],
+        #         "QuestionAnalysis":result["QuestionAnalysis"],
+        #         "selectAnswer":result["selectAnswer"],
+        #         "paperid":paperid,
+        #         "planid":planid,
+        #         "questionId":questionId
+        #     }
+        #     with open("./xiti.jsonl","a",encoding="utf-8") as f:
+        #         f.write(json.dumps(result,ensure_ascii=False))
+        #         f.write("\n")
